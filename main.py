1538f197c3ce8e5ecb5fe174ee51237a639efb3f
pooopkaaa <60960322+pooopkaaa@users.noreply.github.com>
2021-05-24 09:03:42 +0300

    Fix repeated call response.json

--- main.py
+++ main.py
@@ -13,8 +13,10 @@ def get_response_get(url, payload=None):
 
 def get_response_post(url, files=None, payload=None):
     response = requests.post(url, files=files, data=payload)
-    check_post_response(response)
-    return response
+    response.raise_for_status()
+    converted_response = response.json()
+    check_post_response(converted_response)
+    return converted_response
 
 
 def get_comic_description(comic_number):
@@ -24,10 +26,8 @@ def get_comic_description(comic_number):
 
 
 def check_post_response(response):
-    response.raise_for_status()
-    response_dict = response.json()
-    if 'error' in response_dict:
-        raise requests.HTTPError(response_dict['error']['error_code'])
+    if 'error' in response:
+        raise requests.HTTPError(response['error']['error_code'])
 
 
 def get_comics_amount():
@@ -45,7 +45,7 @@ def download_comic_image(filename, url):
 def get_url_for_upload_image(**payload):
     url = 'https://api.vk.com/method/photos.getWallUploadServer'
     response = get_response_post(url, payload=payload)
-    return response.json()['response']['upload_url']
+    return response['response']['upload_url']
 
 
 def upload_image(url, filename):
@@ -54,19 +54,19 @@ def upload_image(url, filename):
             'photo': file,
         }
         response = get_response_post(url, files=files)
-        return response.json()
+        return response
 
 
 def lock_uploaded_image(**payload):
     url = 'https://api.vk.com/method/photos.saveWallPhoto'
-    response = get_response_post(url, payload=payload).json()['response'][0]
+    response = get_response_post(url, payload=payload)['response'][0]
     return response['id'], response['owner_id']
 
 
 def publish_uploaded_image(**payload):
     url = 'https://api.vk.com/method/wall.post'
     response = get_response_post(url, payload=payload)
-    return response.json()['response']['post_id']
+    return response['response']['post_id']
 
 
 def main():
